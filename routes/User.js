6747ad8c822d1edcfc7fca743b8666c2642a2987
tanmaysihare <tanmaysihare@gmail.com>
2024-03-27 03:42:43 +0530

    bcrypt password

--- routes/User.js
+++ routes/User.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require('../controller/User');
 
 router.post("/signup", userController.postSignup);
-router.post("/login",userController.postLogin);
+router.post("/login",userController.postLogin2);
 
 module.exports = router;
